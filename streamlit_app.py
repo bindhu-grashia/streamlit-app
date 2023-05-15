8884db01aa3151d5b83d8d68110b33b8539f8d18
bindhu-grashia <133642621+bindhu-grashia@users.noreply.github.com>
2023-05-15 14:20:56 +0530

    Update streamlit_app.py
    adding text about diner

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import streamlit
+streamlit.title('My parents dine')
+streamlit.header('Menu')
+streamlit.text('Egss and Omelete')
+streamlit.text('Egss - Scrambled')
+streamlit.text('Egss - Poached')
+streamlit.text('Egss - Fried')
 
