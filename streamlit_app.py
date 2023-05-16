8fe2a66ec7457b8758a6049aec0d7db6f37f0ee8
bindhu-grashia <133642621+bindhu-grashia@users.noreply.github.com>
2023-05-16 16:13:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,5 @@ fruits_selected= streamlit.multiselect('Pick some fruits: ' ,list(my_fruit_list.
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+streamlit.text('https://docs.google.com/spreadsheets/d/1KVmyYT992-NzmQA2jojb1o7RMUI0o0nGsFMRbVqRVhg/edit?resourcekey#gid=1372470853')
+
