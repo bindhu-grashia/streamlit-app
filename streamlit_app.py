a190622b06bad251bdeb1c35ba476c22bc0f355c
bindhu-grashia <133642621+bindhu-grashia@users.noreply.github.com>
2023-06-21 14:59:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 import pandas
 import requests
+streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 streamlit.title('My parents dine')
 streamlit.header('🍞Menu')
